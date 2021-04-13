cfb354ea0b8f91515c99c77e640306493697b839
linocks <knocksto1@gmail.com>
2021-04-13 08:12:30 +0000

    modified jetty server to run on a separate thread

--- src/main/java/io/turntabl/NewRelicJavaProfiler.java
+++ src/main/java/io/turntabl/NewRelicJavaProfiler.java
@@ -5,7 +5,11 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import io.turntabl.jetty.JettyServer;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NewRelicJavaProfiler implements ToolWindowFactory {
 
@@ -15,5 +19,9 @@ public class NewRelicJavaProfiler implements ToolWindowFactory {
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(newRelicJavaProfilerToolWindow.getContent(), "NewRelic Profiler", false);
         toolWindow.getContentManager().addContent(content);
+
+        //run the jectey server
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(new JettyServer());
     }
 }
--- src/main/java/io/turntabl/jetty/JettyServer.java
+++ src/main/java/io/turntabl/jetty/JettyServer.java
@@ -3,8 +3,9 @@ package io.turntabl.jetty;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 
-public class JettyServer {
-    public static void main(String[] args) {
+public class JettyServer implements Runnable {
+    @Override
+    public void run() {
         Server server = new Server(8787);
         ServletHandler handler = new ServletHandler();
         handler.addServletWithMapping(io.turntabl.jetty.MetricHandler.class, "/metrics");
@@ -12,10 +13,11 @@ public class JettyServer {
 
         try {
             server.start();
+            System.out.println("Jetty server started on port 8787");
             server.join();
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
 
+    }
 }
