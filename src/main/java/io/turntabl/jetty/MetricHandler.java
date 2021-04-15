aa49b55dfb451891db9b0c8727bc6ddd29a4d26c
linocks <knocksto1@gmail.com>
2021-04-15 16:52:04 +0000

    implemented updating of plugin ui with payload received by jetty server

--- src/main/java/io/turntabl/jetty/MetricHandler.java
+++ src/main/java/io/turntabl/jetty/MetricHandler.java
@@ -1,16 +1,42 @@
 package io.turntabl.jetty;
 
+import com.intellij.ui.components.JBTabbedPane;
+import io.turntabl.NewRelicJavaProfilerToolWindow;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class MetricHandler extends HttpServlet {
+    private final NewRelicJavaProfilerToolWindow toolWindowComponent;
+    Logger logger = Logger.getLogger(MetricHandler.class.getName());
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public MetricHandler() {
+        toolWindowComponent = null;
+    }
+
+    public MetricHandler(NewRelicJavaProfilerToolWindow toolWindowComponent) {
+        this.toolWindowComponent = toolWindowComponent;
+    }
+
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().println("<h1> Welcome to Jetty Server </h1>");
+        String payload = req.getReader().lines().collect(Collectors.joining());
+        logger.info(payload);
+
+        toolWindowComponent.setEventlableText(payload);
+        toolWindowComponent.getContent().repaint();
+        toolWindowComponent.getContent().validate();
+
+        resp.getWriter().println("acknowledgement: data received successfully");
     }
+
+
 }
