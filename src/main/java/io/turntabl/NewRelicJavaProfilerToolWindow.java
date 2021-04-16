e5fca9d30a8e9afec7bd0279ae335a495ec9603b
linocks <knocksto1@gmail.com>
2021-04-16 15:21:13 +0000

    changed events panel to diplay text with textarea instead of label

--- src/main/java/io/turntabl/NewRelicJavaProfilerToolWindow.java
+++ src/main/java/io/turntabl/NewRelicJavaProfilerToolWindow.java
@@ -2,7 +2,10 @@ package io.turntabl;
 
 import com.intellij.openapi.vcs.changes.PreviewDiffSplitterComponent;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.JBTabbedPane;
+import com.intellij.ui.components.JBTextArea;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,6 +16,8 @@ public class NewRelicJavaProfilerToolWindow {
     private JPanel mainPanel, flameGraphPanel, callTreePanel, methodListPanel, eventsPanel;
     private PreviewDiffSplitterComponent splitterComponent;
     private JTabbedPane tabbedPane;
+    private JTextArea eventTextArea;
+    private JScrollPane scrollPane;
 
     public NewRelicJavaProfilerToolWindow(ToolWindow toolWindow) {
         label = new JLabel("NewRelic Profiler");
@@ -23,6 +28,11 @@ public class NewRelicJavaProfilerToolWindow {
         callTreePanel = new JPanel();
         methodListPanel = new JPanel();
         eventsPanel = new JPanel();
+        eventTextArea = new JBTextArea(5,200);
+        eventTextArea.setLineWrap(true);
+
+
+        scrollPane = new JBScrollPane(eventTextArea);
 
         flameGraphLabel = new JLabel("Flame Graph is displayed here!!!!!!!!!!!");
         callTreeLabel = new JLabel("Call Tree is displayed here!!!!!!!!!!!");
@@ -32,7 +42,16 @@ public class NewRelicJavaProfilerToolWindow {
         flameGraphPanel.add(flameGraphLabel);
         callTreePanel.add(callTreeLabel);
         methodListPanel.add(methodListLabel);
-        eventsPanel.add(eventsLabel);
+
+        eventTextArea.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim" +
+                " ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
+                "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate" +
+                " velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        eventTextArea.setBackground(eventsPanel.getBackground());
+//        eventsPanel.add(eventsLabel);//adds events label
+        eventsPanel.add(scrollPane);//adds scrollpane with text area
 
         tabbedPane = new JBTabbedPane();
 
@@ -40,6 +59,7 @@ public class NewRelicJavaProfilerToolWindow {
         tabbedPane.add("Call Tree", callTreePanel);
         tabbedPane.add("Method List", methodListPanel);
         tabbedPane.add("Events", eventsPanel);
+        tabbedPane.setSelectedIndex(3);
 
         mainPanel.add(tabbedPane);
 
@@ -49,9 +69,10 @@ public class NewRelicJavaProfilerToolWindow {
         label.setText("Hello New Relic");
     }
 
-    public void setEventlableText(String text){
-        eventsLabel.setText(text);
+    public void setEventlabelText(String text) {
+        eventTextArea.setText(text);
     }
+
     public JComponent getContent() {
         return mainPanel;
     }
--- src/main/java/io/turntabl/jetty/MetricHandler.java
+++ src/main/java/io/turntabl/jetty/MetricHandler.java
@@ -1,13 +1,11 @@
 package io.turntabl.jetty;
 
-import com.intellij.ui.components.JBTabbedPane;
 import io.turntabl.NewRelicJavaProfilerToolWindow;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.*;
 import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -31,7 +29,7 @@ public class MetricHandler extends HttpServlet {
         String payload = req.getReader().lines().collect(Collectors.joining());
         logger.info(payload);
 
-        toolWindowComponent.setEventlableText(payload);
+        toolWindowComponent.setEventlabelText(payload);
         toolWindowComponent.getContent().repaint();
         toolWindowComponent.getContent().validate();
 
