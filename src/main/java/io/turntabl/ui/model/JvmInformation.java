3aeed49630ebb92e8b45b296cc6299546cb67f4c
Zaneta Asare <zaneta.asare@turntabl.io>
2021-04-26 12:51:39 +0000

    fixed a few typos in jvmInformation model

--- src/main/java/io/turntabl/ui/model/JvmInformation.java
+++ src/main/java/io/turntabl/ui/model/JvmInformation.java
@@ -1,5 +1,7 @@
 package io.turntabl.ui.model;
 
+import java.util.Date;
+
 public class JvmInformation {
     private Date timeStamp;
     private String jvmProperty;
@@ -7,52 +9,36 @@ public class JvmInformation {
     private String instrumentationName;
     private String hostName;
     private String collectorName;
-    private String instrumentionProvider;
+    private String instrumentationProvider;
 
 
-    public JvmInformation(Date timeStamp, String jvmProperty, String jvmPropertyValue, String instrumentationName, String hostName, String collectorName, String instrumentionProvider) {
+    public JvmInformation(Date timeStamp, String jvmProperty, String jvmPropertyValue, String instrumentationName, String hostName, String collectorName, String instrumentationProvider) {
         this.timeStamp = timeStamp;
         this.jvmProperty = jvmProperty;
         this.jvmPropertyValue = jvmPropertyValue;
         this.instrumentationName = instrumentationName;
         this.hostName = hostName;
         this.collectorName = collectorName;
-        this.instrumentionProvider = instrumentionProvider
+        this.instrumentationProvider = instrumentationProvider;
     }
 
     public Date getTimeStamp() {
         return timeStamp;
     }
 
-//    public void settimeStamp(String time) {
-//        this.timeStamp = time;
-//    }
-
     public String getJvmProperty() {
-
         return jvmProperty;
     }
 
-//    public void setjvmProperty(String jvmproperty) {
-//        this.jvmProperty = jvmproperty;
-//    }
-
     public String getJvmPropertyValue() {
-
         return jvmPropertyValue;
     }
 
-    //public void setTotal(String total) {
-        this.total = total;
-    }
-
     public String getInstrumentationName() {
-
         return instrumentationName;
     }
 
     public String getHostName() {
-
         return hostName;
     }
 
@@ -60,10 +46,7 @@ public class JvmInformation {
         return collectorName;
     }
 
-    public String getInstrumentionProvider() {
-        return instrumentionProvider;
+    public String getInstrumentationProvider() {
+        return instrumentationProvider;
     }
-
-
-
 }
