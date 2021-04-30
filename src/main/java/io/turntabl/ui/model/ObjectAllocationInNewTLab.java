ec20a92acac7a555f7ae2d0752bf5438988d5c1d
Yaa-Providence-Design <72032437+Yaa-Providence-Design@users.noreply.github.com>
2021-04-30 15:10:15 +0000

    object allocation in new TLab and outsideTLab (#23)
    Co-authored-by: Yaa-Providence-Design <providence.annor@turntabl.io>

---
+++ src/main/java/io/turntabl/ui/model/ObjectAllocationInNewTLab.java
@@ -0,0 +1,24 @@
+package io.turntabl.ui.model;
+
+import java.util.HashMap;
+
+public class ObjectAllocationInNewTLab extends  ObjectAllocation{
+    public ObjectAllocationInNewTLab() {
+    }
+
+    public ObjectAllocationInNewTLab(String name, String type, HashMap<String, Double> value, Long timestamp, int interval, HashMap<String, String> attributes) {
+        super(name, type, value, timestamp, interval, attributes);
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectAllocationInNewTLab{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", value=" + value +
+                ", timestamp=" + timestamp +
+                ", interval=" + interval +
+                ", attributes=" + attributes +
+                '}';
+    }
+}
