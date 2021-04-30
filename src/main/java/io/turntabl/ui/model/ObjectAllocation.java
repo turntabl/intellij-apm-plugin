3ec1ff0f48d03d84be25f0e1f314fb504549d39f
Yaa-Providence-Design <72032437+Yaa-Providence-Design@users.noreply.github.com>
2021-04-30 15:10:15 +0000

    object allocation in new TLab and outsideTLab (#23)
    Co-authored-by: Yaa-Providence-Design <providence.annor@turntabl.io>

---
+++ src/main/java/io/turntabl/ui/model/ObjectAllocation.java
@@ -0,0 +1,85 @@
+package io.turntabl.ui.model;
+
+import java.util.HashMap;
+
+public class ObjectAllocation {
+    protected String name;
+    protected String type;
+    protected HashMap<String, Double> value;
+    protected Long timestamp;
+    protected int interval;
+    protected HashMap<String, String> attributes;
+
+
+    public ObjectAllocation() {
+    }
+
+    public ObjectAllocation(String name, String type, HashMap<String, Double> value, Long timestamp, int interval, HashMap<String, String> attributes) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+        this.timestamp = timestamp;
+        this.interval = interval;
+        this.attributes = attributes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public HashMap<String, Double> getValue() {
+        return value;
+    }
+
+    public void setValue(HashMap<String, Double> value) {
+        this.value = value;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public HashMap<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(HashMap<String, String> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectAllocation{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", value=" + value +
+                ", timestamp=" + timestamp +
+                ", interval=" + interval +
+                ", attributes=" + attributes +
+                '}';
+    }
+}
