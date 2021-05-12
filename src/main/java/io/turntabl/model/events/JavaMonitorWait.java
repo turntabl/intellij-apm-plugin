cda17d6ba01e5f9c86a255b5142d3e82547587ac
Yaa-Providence-Design <providence.annor@turntabl.io>
2021-05-12 18:02:02 +0000

    JavaMonitorWait view created, junit test created for javamonitorwait added

--- src/main/java/io/turntabl/model/events/JavaMonitorWait.java
+++ src/main/java/io/turntabl/model/events/JavaMonitorWait.java
@@ -1,129 +1,63 @@
 package io.turntabl.model.events;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.turntabl.model_template.AbstractEventsInfo;
+
 import java.util.HashMap;
 
-public class JavaMonitorWait {
-    private String startTime;
-    private String duration;
-    private String endTime;
-    private String eventThread;
-    private String monitorClass;
-    private String notifierThread;
-    private String timeOut;
-    private String timedOut;
-    private String monitorAddress;
-    private HashMap<String, String> attributes;
-
-
-    public JavaMonitorWait(String startTime, String duration, String endTime, String eventThread, String monitorClass, String notifierThread, String timeOut, String timedOut, String monitorAddress, HashMap<String, String> attributes) {
-        this.startTime = startTime;
-        this.duration = duration;
-        this.endTime = endTime;
-        this.eventThread = eventThread;
-        this.monitorClass = monitorClass;
-        this.notifierThread = notifierThread;
-        this.timeOut = timeOut;
-        this.timedOut = timedOut;
-        this.monitorAddress = monitorAddress;
-        this.attributes = attributes;
-    }
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JavaMonitorWait extends AbstractEventsInfo {
+    private int duration;
+    private String stackTrace;
+    @JsonProperty("thread.name")
+    private String threadName;
+    @JsonProperty("class")
+    private String className;
 
     public JavaMonitorWait() {
+        super();
     }
 
-    public HashMap<String, String> getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(HashMap<String, String> attributes) {
-        this.attributes = attributes;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getDuration() {
+    public int getDuration() {
         return duration;
     }
 
-    public void setDuration(String duration) {
+    public void setDuration(int duration) {
         this.duration = duration;
     }
 
-    public String getEndTime() {
-        return endTime;
+    public String getStackTrace() {
+        return stackTrace;
     }
 
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
     }
 
-    public String getEventThread() {
-        return eventThread;
+    public String getThreadName() {
+        return threadName;
     }
 
-    public void setEventThread(String eventThread) {
-        this.eventThread = eventThread;
+    public void setThreadName(String threadName) {
+        this.threadName = threadName;
     }
 
-    public String getMonitorClass() {
-        return monitorClass;
+    public String getClassName() {
+        return className;
     }
 
-    public void setMonitorClass(String monitorClass) {
-        this.monitorClass = monitorClass;
-    }
-
-    public String getNotifierThread() {
-        return notifierThread;
-    }
-
-    public void setNotifierThread(String notifierThread) {
-        this.notifierThread = notifierThread;
-    }
-
-    public String getTimeOut() {
-        return timeOut;
-    }
-
-    public void setTimeOut(String timeOut) {
-        this.timeOut = timeOut;
-    }
-
-    public String getTimedOut() {
-        return timedOut;
-    }
-
-    public void setTimedOut(String timedOut) {
-        this.timedOut = timedOut;
-    }
-
-    public String getMonitorAddress() {
-        return monitorAddress;
-    }
-
-    public void setMonitorAddress(String monitorAddress) {
-        this.monitorAddress = monitorAddress;
+    public void setClassName(String className) {
+        this.className = className;
     }
 
     @Override
     public String toString() {
         return "JavaMonitorWait{" +
-                "startTime='" + startTime + '\'' +
-                ", duration='" + duration + '\'' +
-                ", endTime='" + endTime + '\'' +
-                ", eventThread='" + eventThread + '\'' +
-                ", monitorClass='" + monitorClass + '\'' +
-                ", notifierThread='" + notifierThread + '\'' +
-                ", timeOut='" + timeOut + '\'' +
-                ", timedOut='" + timedOut + '\'' +
-                ", monitorAddress='" + monitorAddress + '\'' +
-                ", attributes=" + attributes +
+                "duration=" + duration +
+                ", stackTrace='" + stackTrace + '\'' +
+                ", threadName='" + threadName + '\'' +
+                ", className='" + className + '\'' +
                 '}';
     }
 }
---
+++ src/test/java/io/turntabl/utils/JavaMonitorWaitUtlTest.java
@@ -0,0 +1,41 @@
+package io.turntabl.utils;
+
+import io.turntabl.model.events.JavaMonitorWait;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JavaMonitorWaitUtilTest extends TestCase {
+    private static JsonUtility jsonUtil;
+    private static JavaMonitorWaitUtil javaMonitorWaitUtil;
+    private static String eventsJson;
+
+
+    @BeforeAll
+    public static void setUpAll() {
+        jsonUtil = new JsonUtility();
+        javaMonitorWaitUtil = new JavaMonitorWaitUtil(jsonUtil);
+        eventsJson = "[{\n" +
+                "        \"eventType\": \"JfrJavaMonitorWait\", \n" +
+                "         \"timestamp\": 1619441645494,\n" +
+                "          \"duration\": 28, \n" +
+                "           \"stackTrace\": {\"type\":\"stacktrace\",\"language\":\"java\",\"version\":1,\"truncated\":false,\"payload\":[{\"desc\":\"java.lang.Object.wait(J)V\",\"line\":\"-1\",\"bytecodeIndex\":\"0\"},{\"desc\":\"jdk.jfr.internal.PlatformRecorder.takeNap(J)V\",\"line\":\"448\",\"bytecodeIndex\":\"24\"},{\"desc\":\"jdk.jfr.internal.PlatformRecorder.periodicTask()V\",\"line\":\"441\",\"bytecodeIndex\":\"51\"},{\"desc\":\"jdk.jfr.internal.PlatformRecorder.lambda$startDiskMonitor$1()V\",\"line\":\"386\",\"bytecodeIndex\":\"1\"},{\"desc\":\"jdk.jfr.internal.PlatformRecorder$$Lambda$118.778155119.run()V\",\"line\":\"-1\",\"bytecodeIndex\":\"4\"},{\"desc\":\"java.lang.Thread.run()V\",\"line\":\"834\",\"bytecodeIndex\":\"11\"}]}," + " \n" +
+                "           \"instrumentation.name\": \"JFR\", \n" +
+                "           \"host.hostname\": \"DESKTOP-8AO41P3/172.21.224.1\", \n" +
+                "            \"thread.name\": \"JFR Periodic Tasks\", \n" +
+                "            \"class\": \"java.lang.Object\", \n" +
+                "            \"collector.name\": \"JFR-Uploader\", \n" +
+                "            \"instrumentation.provider\": \"JFR-Uploader\", \n" +
+                "}]";
+    }
+
+    @Test
+    void getJavaMonitorWaitList() {
+        List<JavaMonitorWait> eventList = javaMonitorWaitUtil.getJavaMonitorWaitList(eventsJson);
+        assertEquals(1, eventList.size());
+    }
+}
