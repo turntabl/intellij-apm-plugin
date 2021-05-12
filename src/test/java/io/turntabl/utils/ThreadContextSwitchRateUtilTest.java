0cd9d6f95115657d85985bbf1f3da1b3a8a2634f
justTurn <82210670+justTurn@users.noreply.github.com>
2021-05-12 16:49:00 +0000

    Summary metaspace util test (#46)
    * created a test for SummaryMetaspaceUtil
    
    * added Summary Metaspace and ThreadCOntextSwitchRate Util Tests
    
    * resolved testing issues
    
    * changed back SummaryMetaspace consolitedTest function to assert with 1

---
+++ src/test/java/io/turntabl/utils/ThreadContextSwitchRateUtilTest.java
@@ -0,0 +1,66 @@
+package io.turntabl.utils;
+
+import io.turntabl.model.metrics.ThreadContextSwitchRate;
+import org.json.simple.JSONArray;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThreadContextSwitchRateUtilTest {
+
+    private static ThreadContextSwitchRateUtil threadContextSwitchRateUtil;
+    private static JsonUtility jsonUtility;
+    private static String exampleJsonMetric;
+    private static Optional<JSONArray> jsonArray;
+
+    @BeforeAll
+    public static void setupAll() {
+        System.out.println("Pre Testing Setup running.....");
+        jsonUtility = new JsonUtility();
+        threadContextSwitchRateUtil = new ThreadContextSwitchRateUtil(jsonUtility);
+        exampleJsonMetric = "[\n" +
+                "    {\n" +
+                "        \"common\": {\n" +
+                "            \"attributes\": {\n" +
+                "                \"instrumentation.name\": \"JFR\",\n" +
+                "                \"host.hostname\": \"DESKTOP-8AO41P3/172.21.224.1\",\n" +
+                "                \"collector.name\": \"JFR-Uploader\",\n" +
+                "                \"instrumentation.provider\": \"JFR-Uploader\"\n" +
+                "            }\n" +
+                "        },\n" +
+                "        \"metrics\": [\n" +
+                "            {\n" +
+                "                \"name\": \"jfr.ThreadContextSwitchRate\",\n" +
+                "                \"type\": \"gauge\",\n" +
+                "                \"value\": 21703.333984375,\n" +
+                "                \"timestamp\": 1619441634798,\n" +
+                "                \"attributes\": {}\n" +
+                "            }" +
+                "]\n" +
+                "}\n" +
+                "]";
+
+        jsonArray = jsonUtility.readMetricsJson(exampleJsonMetric);
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getThreadContextSwitchRate() {
+        List<ThreadContextSwitchRate> threadContextSwitchRateList = threadContextSwitchRateUtil.getThreadContextSwitchRate(jsonArray.get());
+        assertEquals(1,threadContextSwitchRateList.size());
+    }
+
+}
