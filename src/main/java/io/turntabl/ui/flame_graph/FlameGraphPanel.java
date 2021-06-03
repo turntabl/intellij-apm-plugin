b614cf7046dbe56598f935fa391f755556714380
Zaneta Asare <zaneta.asare@turntabl.io>
2021-06-03 10:44:37 +0000

    a few modifications

--- src/main/java/io/turntabl/ui/flame_graph/FlameGraphPanel.java
+++ src/main/java/io/turntabl/ui/flame_graph/FlameGraphPanel.java
@@ -25,11 +25,13 @@ public class FlameGraphPanel {
             JSVGCanvas svg = new JSVGCanvas();
             svg.setURI("file:/C:/flamegraph/java_trace.svg");
             svg.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC);
+            svg.setSize(500, 500);
+            panel.setSize(500, 500);
             panel.add(svg, BorderLayout.CENTER);
         }
     }
 
     public JComponent getComponent() {
-        return new JBScrollPane(this.panel, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        return new JBScrollPane(this.panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     }
 }
--- src/main/java/io/turntabl/ui/flame_graph/FlameGraphWithoutThreadNamesPanel.java
+++ src/main/java/io/turntabl/ui/flame_graph/FlameGraphWithoutThreadNamesPanel.java
@@ -3,12 +3,8 @@ package io.turntabl.ui.flame_graph;
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.JBTextArea;
-import org.apache.batik.dom.svg.SVGDOMImplementation;
-import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.swing.JSVGCanvas;
-import org.apache.batik.swing.svg.SVGLoadEventDispatcherAdapter;
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
+
 
 
 import javax.swing.*;
@@ -28,12 +24,13 @@ public class FlameGraphWithoutThreadNamesPanel {
             JSVGCanvas svg = new JSVGCanvas();
             svg.setURI("file:/C:/flamegraph/java_trace_no_thread_name.svg");
             svg.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC);
-
+            svg.setSize(500, 500);
+            panel.setSize(500, 500);
             panel.add(svg, BorderLayout.CENTER);
         }
     }
 
     public JComponent getComponent() {
-        return new JBScrollPane(this.panel, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        return new JBScrollPane(this.panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     }
 }
