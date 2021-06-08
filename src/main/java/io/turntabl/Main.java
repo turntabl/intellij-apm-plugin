0ce28b25ed94e6501d2d5558cad7b38e77d5490f
Justice Essuman <justice.essuman@turntabl.io>
2021-06-08 10:55:19 +0000

    updated gitignore

---
+++ src/main/java/io/turntabl/Main.java
@@ -0,0 +1,16 @@
+package io.turntabl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+        List<Integer> arrList = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++){
+            arrList.add(i);
+            Thread.sleep(2000);
+        }
+    }
+}
