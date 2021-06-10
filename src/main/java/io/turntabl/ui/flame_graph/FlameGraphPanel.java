3394e2d2a3a4ee56500948983b69de92845d74c6
Zaneta Asare <zaneta.asare@turntabl.io>
2021-06-10 17:23:28 +0000

    use of d3 library to show sample flame graph in panel

--- src/main/java/io/turntabl/ui/flame_graph/FlameGraphPanel.java
+++ src/main/java/io/turntabl/ui/flame_graph/FlameGraphPanel.java
@@ -1,37 +1,25 @@
 package io.turntabl.ui.flame_graph;
 
-import com.intellij.ui.components.JBLabel;
+
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
-import com.intellij.ui.components.JBTextArea;
-import org.apache.batik.swing.JSVGCanvas;
-import org.apache.batik.swing.svg.SVGLoadEventDispatcherAdapter;
+import com.intellij.ui.jcef.JBCefBrowser;
 
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
+
 
 public class FlameGraphPanel {
     private JBPanel panel;
 
     public FlameGraphPanel() {
         panel = new JBPanel(new BorderLayout());
-
-        File file = new File("C:/flamegraph/java_trace.svg");
-        if (!file.exists()) {
-            panel.add(new JBTextArea("No flame graph generated yet"), BorderLayout.CENTER);
-        } else {
-            JSVGCanvas svg = new JSVGCanvas();
-            svg.setURI("file:/C:/flamegraph/java_trace.svg");
-            svg.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC);
-            svg.setSize(500, 500);
-            panel.setSize(500, 500);
-            panel.add(svg, BorderLayout.CENTER);
-        }
+        panel.add(new JBCefBrowser("http://localhost:8787/flame-graph").getComponent(), BorderLayout.CENTER);
+        panel.setVisible(true);
     }
 
     public JComponent getComponent() {
-        return new JBScrollPane(this.panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        return new JBScrollPane(this.panel, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     }
 }
--- src/main/java/io/turntabl/ui/flame_graph/FlameGraphWithoutThreadNamesPanel.java
+++ src/main/java/io/turntabl/ui/flame_graph/FlameGraphWithoutThreadNamesPanel.java
@@ -2,35 +2,21 @@ package io.turntabl.ui.flame_graph;
 
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
-import com.intellij.ui.components.JBTextArea;
-import org.apache.batik.swing.JSVGCanvas;
-
-
+import com.intellij.ui.jcef.JBCefBrowser;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
 
 public class FlameGraphWithoutThreadNamesPanel {
     private JBPanel panel;
 
     public FlameGraphWithoutThreadNamesPanel() {
         panel = new JBPanel(new BorderLayout());
-
-        File file = new File("C:/flamegraph/java_trace_no_thread_name.svg");
-        if (!file.exists()) {
-            panel.add(new JBTextArea("No flame graph generated yet"), BorderLayout.CENTER);
-        } else {
-            JSVGCanvas svg = new JSVGCanvas();
-            svg.setURI("file:/C:/flamegraph/java_trace_no_thread_name.svg");
-            svg.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC);
-            svg.setSize(500, 500);
-            panel.setSize(500, 500);
-            panel.add(svg, BorderLayout.CENTER);
-        }
+        panel.add(new JBCefBrowser("http://localhost:8787/flame-graph-no-thread-names/").getComponent(), BorderLayout.CENTER);
+        panel.setVisible(true);
     }
 
     public JComponent getComponent() {
-        return new JBScrollPane(this.panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        return new JBScrollPane(this.panel, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     }
 }
