4e8b6ddc3833e7f4cf5e6e3b1c19111433573638
Justice Essuman <justice.essuman@turntabl.io>
2021-06-28 09:42:52 +0000

    added jfr-deamon jar

--- src/main/java/io/turntabl/Main.java
+++ src/main/java/io/turntabl/Main.java
@@ -1,16 +1,15 @@
-package io.turntabl;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Main {
-
-    public static void main(String[] args) throws InterruptedException {
-	// write your code here
-        List<Integer> arrList = new ArrayList<>();
-        for (int i = 0; i < 1000000; i++){
-            arrList.add(i);
-            Thread.sleep(2000);
-        }
-    }
-}
+//package io.turntabl;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Main {
+//    public static void main(String[] args) throws InterruptedException {
+//	// write your code here
+//        List<Integer> arrList = new ArrayList<>();
+//        for (int i = 0; i < 1000000; i++){
+//            arrList.add(i);
+//            Thread.sleep(2000);
+//        }
+//    }
+//}
